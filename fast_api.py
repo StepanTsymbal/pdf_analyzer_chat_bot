2ff5dfbb081dbf37aafcfab04b9b3273a621dd34
StepanTsymbal <tsymbalstepan@gmail.com>
2024-09-30 12:28:08 +0300

    lifespan instead of on_event; rate limits added

--- fast_api.py
+++ fast_api.py
@@ -1,24 +1,35 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, Request
+from contextlib import asynccontextmanager
 import uvicorn
+from slowapi import Limiter
+from slowapi.util import get_remote_address
 
 from helpers import fast_api_helper
 
-app = FastAPI()
 
-
-@app.on_event("startup")
-async def startup_event():
-    # TODO: move to lifespan?
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     fast_api_helper.init()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+limiter = Limiter(key_func=get_remote_address)
+
+# @app.on_event("startup")
+# async def startup_event():
+#     fast_api_helper.init()
 
 
 @app.get("/docs/")
-async def get_all_docs():
+@limiter.limit("60/minute")
+async def get_all_docs(request: Request):
     return fast_api_helper.get_all_documents()
 
 
 @app.post("/docs/index")
-async def index_doc(file: UploadFile):
+@limiter.limit("30/minute")
+async def index_doc(file: UploadFile, request: Request):
     await fast_api_helper.index_doc(file)
 
     return 'Ok'
